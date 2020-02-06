b099e280cbe4d6e6a642d3422171588c5c9b1e93
huxaiphaer <huxaiphaeridris@gmail.com>
2020-02-06 23:31:27 +0300

    add navbar & footer

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+    return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <div className="navbar-header light">
+              MovieSeriesInfo
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar
---
+++ src/utils/ApiKey.js
@@ -0,0 +1,3 @@
+module.exports ={
+    APIKey: 'f3a05026119d09f84c9aaef927a18ac2'
+}
